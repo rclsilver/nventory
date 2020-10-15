3c8f1feae574f569795bbcee5ab84290f4f40af4
Thomas Bétrancourt <rclsilver@gmail.com>
2020-10-15 18:07:50 +0200

    [auth] Create a dummy external authentication (#2)

--- controllers/debug.go
+++ controllers/debug.go
@@ -9,3 +9,9 @@ func DebugHeaders(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
   JSONEncode(w, r.Header)
 }
+
+func DebugCurrentUser(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+  w.WriteHeader(http.StatusOK)
+  JSONEncode(w, r.Context().Value("user"))
+}
